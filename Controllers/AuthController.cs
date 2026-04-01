f8eebcafcc7feeac1c18453b6a5de780024de1b9
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-01 15:25:15 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using PassAuth.DTOs;
+using PassAuth.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PassAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        public static User user = new();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return Unauthorized("Invalid username or password.");
+            }
+
+            var hasher = new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password);
+
+            if (hasher == PasswordVerificationResult.Failed)
+            {
+                return Unauthorized("Invalid username or password.");
+            }
+            return Ok("Login successful.");
+        }
+    }
+}
