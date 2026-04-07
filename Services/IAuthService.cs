c3277249f64b74addb2d20e464b53d7fad6e7c37
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-07 16:45:29 -0300

    feat: add interface authservice

---
+++ Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using PassAuth.DTOs;
+
+namespace PassAuth.Services
+{
+    public interface IAuthService
+    {
+        Task Register(UserDto request);
+        Task<string> Login(UserDto request);
+    }
+}
