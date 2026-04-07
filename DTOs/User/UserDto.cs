b404812b4ccf4ba5c53fc63989c92d1a5f02ca81
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-07 17:40:11 -0300

    feat: add dataannotations em dto de registro de user

--- DTOs/User/UserDto.cs
+++ DTOs/User/UserDto.cs
@@ -1,9 +1,19 @@
-﻿namespace PassAuth.DTOs.User
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PassAuth.DTOs.User
 {
-    public class UserDto
+    public class UserRegisterDto
     {
+        [Required(ErrorMessage = "O nome de usuário é obrigatório.")]
+        [StringLength(20, MinimumLength = 3)]
         public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "E-mail inválido.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres.")]
         public string Password { get; set; } = string.Empty;
-        
     }
 }
