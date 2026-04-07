3cdeb1b41f64fc05fdfb366e99ef3148657e34ba
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-07 17:14:42 -0300

    feat: implementa cadastro em authservice

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,5 +1,9 @@
-﻿using PassAuth.Context;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PassAuth.Context;
 using PassAuth.DTOs;
+using PassAuth.Models;
+using PassAuth.Models.Enums;
 
 namespace PassAuth.Services
 {
@@ -12,14 +16,32 @@ namespace PassAuth.Services
             this.context = context;
         }
 
-        public Task<string> Login(UserDto request)
+        public async Task<string> Login(UserDto request)
         {
             throw new NotImplementedException();
         }
 
-        public Task Register(UserDto request)
+        public async Task<UserResponseDto> Register(UserDto request)
         {
-            throw new NotImplementedException();
+            var newUser = new User
+            {
+                Username = request.Username,
+                Role = UserRole.User
+            };
+
+            var hasher = new PasswordHasher<User>();
+
+            newUser.PasswordHash = hasher.HashPassword(newUser, request.Password);
+
+            context.Users.Add(newUser);
+
+            var response = new UserResponseDto
+            {
+                Username = request.Username,
+            };
+
+            await context.SaveChangesAsync();
+            return response;
         }
     }
 }
