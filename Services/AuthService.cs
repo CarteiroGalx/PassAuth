d031ecd27be06f6c3d5dd86f210b1afff699fadf
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-07 16:47:00 -0300

    feat: implementa service authservice

---
+++ Services/AuthService.cs
@@ -0,0 +1,25 @@
+﻿using PassAuth.Context;
+using PassAuth.DTOs;
+
+namespace PassAuth.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext context;
+
+        public AuthService(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Task<string> Login(UserDto request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Register(UserDto request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
