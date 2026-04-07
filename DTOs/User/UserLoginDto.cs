e21bb7c4da82a5f2a916b2907a5dcce31d15b740
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-07 17:44:12 -0300

    feat: implementa dto de login

---
+++ DTOs/User/UserLoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PassAuth.DTOs.User
+{
+    public class UserLoginDto
+    {
+        [Required(ErrorMessage = "O nome de usuário é obrigatório.")]
+        [StringLength(20, MinimumLength = 3)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres.")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
