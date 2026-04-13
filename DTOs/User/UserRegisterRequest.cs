99b941eb05fa2c7f60f4e7ad432d1f559ef594ac
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-13 19:09:01 -0300

    refactor: readiciona novo dto para register

---
+++ DTOs/User/UserRegisterRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PassAuth.DTOs.User
+{
+    public class UserRegisterRequest : UserBaseRequest
+    {
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres.")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
