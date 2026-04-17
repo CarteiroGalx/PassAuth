669e6ea264ae3411ab78dc9499435a1938cbb3fc
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-16 23:14:33 -0300

    feat: add novo controlador accountcontroller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PassAuth.Context;
+using PassAuth.Models;
+using System.Security.Claims;
+
+namespace PassAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AccountController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("me")]
+        public async Task<ActionResult<User>> GetMe()
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(userIdClaim))
+            {
+                return Unauthorized();
+            }
+
+            var id = int.Parse(userIdClaim);
+
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+            if (user == null) return NotFound();
+
+            return Ok(user);
+        }
+    }
+}
