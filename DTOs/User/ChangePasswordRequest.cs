4499cb6f0841c8690ca6ee6041a054e0be1ee293
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-17 00:32:00 -0300

    feat: add valores iniciais para props do dto

--- DTOs/User/ChangePasswordRequest.cs
+++ DTOs/User/ChangePasswordRequest.cs
@@ -5,14 +5,14 @@ namespace PassAuth.DTOs.User
     public class ChangePasswordRequest
     {
         [Required(ErrorMessage = "A senha atual é obrigatória")]
-        public string CurrentPassword { get; set; }
+        public string CurrentPassword { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "A nova senha é obrigatória")]
         [StringLength(100, MinimumLength = 6, ErrorMessage = "A senha deve ter pelo menos 6 caractéres")]
-        public string NewPassword { get; set; }
+        public string NewPassword { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Digite a senha de confirmação")]
         [Compare("CurrentPassword", ErrorMessage = "As senhas não coincidem")]
-        public string ConfirmationPassword { get; set; }
+        public string ConfirmationPassword { get; set; } = string.Empty;
     }
 }
