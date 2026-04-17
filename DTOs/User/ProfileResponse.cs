ee05262a10bb430974323439ffafc5479008b9eb
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-16 23:58:58 -0300

    refactor: remove prop nao referenciada

--- DTOs/User/ProfileResponse.cs
+++ DTOs/User/ProfileResponse.cs
@@ -4,7 +4,6 @@ namespace PassAuth.DTOs.User
 {
     public class ProfileResponse
     {
-        public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty ;
         public UserRole Role { get; set; }
