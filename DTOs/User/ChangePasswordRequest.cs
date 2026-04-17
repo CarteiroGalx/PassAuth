083a890473c561161fcfacd2d63c1f4085511b99
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-17 00:19:56 -0300

    feat: add data annotations no dto de troca de senha

--- DTOs/User/ChangePasswordRequest.cs
+++ DTOs/User/ChangePasswordRequest.cs
@@ -1,6 +1,15 @@
-﻿namespace PassAuth.DTOs.User
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PassAuth.DTOs.User
 {
     public class ChangePasswordRequest
     {
+        [Required(ErrorMessage = "A senha atual é obrigatória")]
+        public string ActualPass { get; set; }
+        [Required(ErrorMessage = "A nova senha é obrigatória")]
+        public string NewPass { get; set; }
+        [Required(ErrorMessage = "Digite a senha de confirmação")]
+        [Compare("ActualPass", ErrorMessage = "As senhas não coincidem")]
+        public string ConfirmationPass { get; set; }
     }
 }
