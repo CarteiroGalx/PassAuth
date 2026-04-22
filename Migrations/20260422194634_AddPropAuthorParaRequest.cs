7c7a97c45dcc5b00925566b8aa7b1925715d8cab
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-22 16:52:16 -0300

    feat: nova migration de adicao de prop do modelo request

---
+++ Migrations/20260422194634_AddPropAuthorParaRequest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PassAuth.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropAuthorParaRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Requests",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Requests");
+        }
+    }
+}
