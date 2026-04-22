630b058db4f144d19bb74755914fbc3f787596e0
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-22 17:09:39 -0300

    feat: add props de tempo no model request

--- Models/ManagerRequest.cs
+++ Models/ManagerRequest.cs
@@ -16,5 +16,8 @@ namespace PassAuth.Models
         public string Description { get; set; } = string.Empty;
         [Required]
         public string Author { get; set; } = string.Empty;
+        [Required]
+        public DateTime PublishedAt { get; set; } = DateTime.UtcNow; 
+        public DateTime? ReviewedAt { get; set; }  
+        }
     }
-}
