d2d0f966ddc7922b99b1aa26542e40d58b0b0060
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-22 17:11:14 -0300

    feat: migration de novas props do request

---
+++ Migrations/20260422201029_AddPropsTempoRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PassAuth.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropsTempoRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublishedAt",
+                table: "Requests",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReviewedAt",
+                table: "Requests",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublishedAt",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "ReviewedAt",
+                table: "Requests");
+        }
+    }
+}
