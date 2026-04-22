515ad8705d04017c9dc06f3dcea02272ae2230ae
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-22 16:36:49 -0300

    fix: ajusta nome de classe mae

--- DTOs/Request/CreateRequestDto.cs
+++ DTOs/Request/CreateRequestDto.cs
@@ -2,6 +2,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PassAuth.DTOs.Request
 {
-    public class CreateRequestDto : BaseRequest
+    public class CreateRequestDto : BaseManagerRequest
     {}
 }
