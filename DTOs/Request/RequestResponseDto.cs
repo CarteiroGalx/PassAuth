4af1fa3ed84899d2ea502aa488aa5ff24ad70536
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-22 16:36:26 -0300

    feat: add id para dto de resposta de request

--- DTOs/Request/RequestResponseDto.cs
+++ DTOs/Request/RequestResponseDto.cs
@@ -4,6 +4,7 @@ namespace PassAuth.DTOs.Request
 {
     public class RequestResponseDto
     {
+        public int Id { get; set; }
         public RequestStatus Status { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
