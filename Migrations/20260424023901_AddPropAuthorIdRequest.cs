6c5f885b719e82f5161dd762539c365d4de1a10c
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-23 23:39:53 -0300

    feat: cria nova migration para id de autor de request

---
+++ Migrations/20260424023901_AddPropAuthorIdRequest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PassAuth.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropAuthorIdRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Requests",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Requests");
+        }
+    }
+}
