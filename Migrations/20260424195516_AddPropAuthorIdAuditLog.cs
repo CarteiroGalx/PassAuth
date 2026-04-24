8339a4af28e73e1b578ef55b2224473201ec6702
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-24 17:20:28 -0300

    feat: cria migration de nova prop do auditlog

---
+++ Migrations/20260424195516_AddPropAuthorIdAuditLog.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PassAuth.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropAuthorIdAuditLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Audit",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Audit");
+        }
+    }
+}
