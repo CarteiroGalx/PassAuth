5810d5ca07fc3139b83ea40725b64a192f8627b9
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-25 19:07:22 -0300

    feat:  cria migration da nova prop do model user

---
+++ Migrations/20260425220451_AddPropIsActiveUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PassAuth.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropIsActiveUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isActive",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isActive",
+                table: "Users");
+        }
+    }
+}
