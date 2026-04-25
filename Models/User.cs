4d4e80058d75d97f1b8dc20f72c88d6d287884b0
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-25 19:18:09 -0300

    feat: remove prop email do modelo user

--- Models/User.cs
+++ Models/User.cs
@@ -5,7 +5,6 @@ using System.ComponentModel.DataAnnotations;
 namespace PassAuth.Models
 {
     [Index(nameof(Username), IsUnique = true)]
-    [Index(nameof(Email), IsUnique = true)]
     public class User
     {
         [Key] 
@@ -14,10 +13,6 @@ namespace PassAuth.Models
         [Required(ErrorMessage = "O nome de usuário é obrigatório.")]
         [StringLength(20, MinimumLength = 3, ErrorMessage = "O usuário deve ter entre 3 e 20 caracteres.")]
         public string Username { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = "O e-mail é obrigatório.")]
-        [EmailAddress(ErrorMessage = "E-mail em formato inválido.")]
-        public string Email { get; set; } = string.Empty;
 
         [Required]
         public string PasswordHash { get; set; } = string.Empty;
