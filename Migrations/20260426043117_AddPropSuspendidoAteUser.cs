c907c89d46e01942234a2314b3074ddff49e27e4
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-04-27 18:35:08 -0300

    feat: cria nova migration para prop suspendeduntil

---
+++ Migrations/20260426043117_AddPropSuspendidoAteUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PassAuth.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropSuspendidoAteUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SuspendedUntil",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SuspendedUntil",
+                table: "Users");
+        }
+    }
+}
